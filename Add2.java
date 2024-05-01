44b265a80e7e560fc10033fa4ef5932ec4901634
ahana0306 <113210516+ahana0306@users.noreply.github.com>
2024-05-01 20:44:48 +0530

    Update Add2.java

--- Add2.java
+++ Add2.java
@@ -1,21 +1,9 @@
-import java.util.Scanner;  
-public class Add2  
+public class Add2 
 {  
-public static void main(String args[])  
+public static void main(String args[])   
 {  
-int x, y, sum;  
-Scanner sc = new Scanner(System.in);  
-System.out.print("Enter the first number: ");  
-x = sc.nextInt();  
-System.out.print("Enter the second number: ");  
-y = sc.nextInt();  
-sum = sum(x, y);  
-System.out.println("The sum of two numbers x and y is: " + sum);  
+int n1 = 225, n2 = 115, sum;  
+sum = n1 + n2;  
+System.out.println("The sum of numbers is: "+sum);  
 }  
-//method that calculates the sum  
-public static int sum(int a, int b)  
-{  
-int sum = a + b;  
-return sum;  
-}  
-}  
+}    
